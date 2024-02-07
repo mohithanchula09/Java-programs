34c5391cdb7e087027730e6081ebb7465d625733
mohith <mohithanchula09@gmail.com>
2024-02-07 21:46:01 +0530

    basic even code

---
+++ Even5.java
@@ -0,0 +1,25 @@
+import java.lang.*;
+import java.util.Scanner;
+
+class Even5
+{
+    public static void main(String[] args) 
+    {
+        System.out.print("number enter cheyi=\t");
+        
+        Scanner scan=new Scanner(System.in);
+        int s1=scan.nextInt();
+        //s1=8
+        //i=0 
+
+        for(int i=0;i<=s1;i++)
+        {
+            if (i%2==0) 
+            {
+                System.out.println(i);
+            }
+        }
+        
+
+    }
+}
